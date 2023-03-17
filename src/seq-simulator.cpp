8e7858c38d135a7a0cf2d9ae17bd25310e3f9334
acgzone <Ikaros1264@gmail.com>
2023-03-17 22:45:13 +0800

    modified:   src/seq-simulator.cpp

--- src/seq-simulator.cpp
+++ src/seq-simulator.cpp
@@ -2,6 +2,9 @@
 #include "world.h"
 #include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
 
 // TASK 1
 
@@ -16,7 +19,33 @@ public:
                                               Vec2 bmin, Vec2 bmax) {
     // TODO: implement a function that builds and returns a quadtree containing
     // particles.
-    return nullptr;
+    std::unique_ptr<QuadTreeNode> This(new QuadTreeNode);
+    This->isLeaf = 0;
+    This->children[0] = nullptr; This->children[1] = nullptr; This->children[2] = nullptr; This->children[3] = nullptr; //set node to null
+    for(auto &p : particles) {
+      if(p.position.x>=bmin.x && p.position.x<=bmax.x && p.position.y>=bmin.y && p.position.y<=bmax.y){
+        This->particles.push_back(p);
+        std::swap(p,particles.back());
+        particles.pop_back();
+      }
+    }
+    if(This->particles.size() > QuadTreeLeafSize){
+      Vec2 bmid((bmin.x + bmax.x) / 2, (bmin.y + bmax.y) / 2);
+      //#pragma omp parallel sections
+      {
+        //#pragma omp section
+        This->children[0] = buildQuadTree(This->particles,bmin,bmid);
+        //#pragma omp section
+        This->children[1] = buildQuadTree(This->particles,(Vec2)(bmid.x,bmin.y),(Vec2)(bmax.x,bmid.y));
+        //#pragma omp section
+        This->children[2] = buildQuadTree(This->particles,(Vec2)(bmin.x,bmid.y),(Vec2)(bmid.x,bmax.y));
+        //#pragma omp section
+        This->children[3] = buildQuadTree(This->particles,bmid,bmax);
+      }
+    }else{
+      This->isLeaf = 1;
+    }
+    return This;
   }
   virtual std::unique_ptr<AccelerationStructure>
   buildAccelerationStructure(std::vector<Particle> &particles) {
@@ -38,7 +67,10 @@ public:
     quadTree->bmax = bmax;
 
     // build nodes
+    std::vector<Particle> cpy(particles);
     quadTree->root = buildQuadTree(particles, bmin, bmax);
+    particles.swap(cpy);
+
     if (!quadTree->checkTree()) {
       std::cout << "Your Tree has Error!" << std::endl;
     }
@@ -51,6 +83,17 @@ public:
                             StepParameters params) override {
     // TODO: implement sequential version of quad-tree accelerated n-body
     // simulation here, using quadTree as acceleration structure
+    newParticles.clear();
+    for(auto &p : particles){
+      Vec2 force = Vec2(0.0f, 0.0f);
+      std::vector<Particle> nearby;
+      accel->getParticles(nearby, p.position, params.cullRadius);
+      for(auto &n : nearby){
+        force += computeForce(p, n, params.cullRadius);
+      }
+      newParticles.push_back(updateParticle(p, force, params.deltaTime));
+    }
+    //newParticles.assign(particles.begin(),particles.end());
   }
 };
 
